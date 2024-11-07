de1d0f53e260cb86d479f0d81d00dbb5e1d9bb7a
tauri_s <tauristclaire@gmail.com>
2024-11-07 12:58:05 -0800

    Add methods to configure database

--- app.py
+++ app.py
@@ -37,6 +37,13 @@ log = logging.getLogger("app")
 # Initialize the Flask application
 app = Flask(__name__)
 
+#* Set up the database
+# SQLALCHEMY_DATABASE_URI sets a database connection URI
+# database accessed at sqlite:///nature_nook.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nature_nook.db'
+# when set to False, interactions with the database to add, update, and delete are not recorded which reduces the overhead
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 #* Create instance of OpenAI class
 llm = ChatOpenAI(
   model="gpt-3.5-turbo",
